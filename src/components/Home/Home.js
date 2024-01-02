71fd90f89c2d5285628c9860454973a2fb07a004
Broderick Boucher <95665786+boucher-broderick@users.noreply.github.com>
2024-01-01 18:20:29 -0700

    image
    image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headshot from "../../Assets/headshot.png";
+import headshot from "../../Assets/babybean.jpg";
 
 
 
