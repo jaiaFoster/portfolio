0844332f912eeb78d1850714fb5c2737038a3f3b
jaiaFoster <jaiafoster10@gmail.com>
2024-01-17 18:24:51 -0500

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,9 +8,10 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            My name is <span className="purple">Jaia Foster</span>, and I'm passionate about teamwork and automation.
+            My name is <span className="purple">Jaia Foster</span>, and I like teamwork and automation.
 
             <br /> I'm completing my senior year at Rennselear Polytechnic Institute (RPI), where I'm majoring in Mechanical Engineering and minoring in Economics.
+            <br />
             <br />
             I've always loved the act of creation, and this act has expanded into many different forms since enrolling at RPI. From dabbling in programming to designing workflows and being a team leader, I've learned that creation needs not always result in a physical object.
             <br />
