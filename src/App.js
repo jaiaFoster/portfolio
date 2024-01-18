41e232a88b7e96cee182253ae610b613ba1cda47
jaiaFoster <jaiafoster10@gmail.com>
2024-01-18 09:37:38 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Footer from "./components/Footer";
-import Resume from "./components/Resume/Gen.Resv9.docx";
+import Resume from "./components/Resume/Gen.Resv9.pdf";
 import {
   BrowserRouter as Router,
   Route,
