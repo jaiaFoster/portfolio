f3bfe0d37dc567c108003e2db93c3f4aae1972dc
jaiaFoster <jaiafoster10@gmail.com>
2024-01-17 20:06:27 -0500

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,30 +2,21 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headshot from "../../Assets/jaiaheadshot.jpg";
 
-
-
 function Home() {
   return (
     <section>
-
-      <Container fluid className="home-section" id="home" style={{ zIndex: 2}}>
+      <Container fluid className="home-section" id="home" style={{ zIndex: 2 }}>
         
-        <Container className="home-content" >
-          <Row >
+        <Container className="home-content">
+          <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!
+                Hello, I'm Jaia
               </h1>
 
               <h1 className="heading-name">
-                I'm
-                <strong className="main-name"> Broderick Boucher</strong>
+                A Passionate <strong className="main-name">Mechanical Engineer</strong>
               </h1>
-
-              <h1 className="heading-name">
-              A Software Developer
-              </h1>
-
             </Col>
 
             <Col md={5} style={{ paddingBottom: 20 }}>
@@ -33,40 +24,25 @@ function Home() {
                 src={headshot}
                 alt="home pic"
                 className="img-home"
-                style={{ maxHeight: "100%", width: "100%", maxWidth: "450px"}}
+                style={{ maxHeight: "300px", width: "auto" }}
               />
             </Col>
-            
           </Row>
         </Container>
 
         <Container>
-        <Row className="home-about-section" >
-          <Col md={8} className="home-about-description" style={{ width: "100%"}}>
-            <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
-            </h1>
-            <p className="home-about-body">
-              I was first exposed to programming in high school and since then, I've been hooked.
-              <br />
-              <br />My journey as a developer began like many programmers before me, with a simple <b className="purple">Java</b> program that printed
-                <b className="purple"> "Hello World" </b> 
-              <br />
-              <br />
-              I am current a college student at RPI studying Computer Science and I have had the opportunity to use a variety of languages and technologies from
-              <b className="purple"> MIPS </b>  to <b className="purple"> React </b> 
-              <br />
-              <br />
-              I am looking forward to applying the knowledge I have gained through my education, internships, and projects to tackle real world problems.
-
-
-            </p>
-          </Col>
-        </Row>
+          <Row className="home-about-section">
+            <Col md={12} className="home-about-description">
+              <h1 style={{ fontSize: "2.6em" }}>
+                LET ME <span className="purple">INTRODUCE</span> MYSELF
+              </h1>
+              <p className="home-about-body">
+                My engineering journey began in high school with the founding of Ellipse 6814 for the FIRST Robotics Competition. This ignited my passion for technical education, teamwork, and the intricacies of automation and design efficiency. As a senior at RPI set to graduate in May, I am eager to bring my academic learnings and hands-on experience to the engineering industry in 2024.
+              </p>
+            </Col>
+          </Row>
+        </Container>
       </Container>
-      </Container>
-
-
     </section>
   );
 }
