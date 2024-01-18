1ae43de662715d9e4b4e191c7134dec7da3aff5b
jaiaFoster <jaiafoster10@gmail.com>
2024-01-17 19:24:50 -0500

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,54 +1,32 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { FaCube, FaShapes, FaFileWord, FaFileExcel, FaFilePowerpoint, FaIndustry, FaPython, FaCode } from "react-icons/fa";
-import { DiMatlab, DiCode, DiTerminal } from "react-icons/di";
-import { GiGearStickPattern } from "react-icons/gi";
+import { FaCube, FaShapes, FaIndustry } from "react-icons/fa";
+import { DiMatlab } from "react-icons/di";
+import { GiMechanicalArm, GiConveyorBelt } from "react-icons/gi";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      {/* Solid Modeling Icons */}
+      {/* Design & Modeling Icons */}
       <Col xs={4} md={2} className="tech-icons">
         <FaCube />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <FaShapes />
       </Col>
-
-      {/* Technologies Icons */}
       <Col xs={4} md={2} className="tech-icons">
-        <FaFileWord />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <FaFileExcel />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <FaFilePowerpoint />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiCode />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiTerminal />
+        <GiMechanicalArm />
       </Col>
 
-      {/* Lab Skills Icons */}
-      <Col xs={4} md={2} className="tech-icons">
-        <FaIndustry />
-      </Col>
+      {/* Analysis & Simulation Icons */}
       <Col xs={4} md={2} className="tech-icons">
         <DiMatlab />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <GiGearStickPattern />
-      </Col>
-
-      {/* Programming Icons */}
-      <Col xs={4} md={2} className="tech-icons">
-        <FaPython />
+        <FaIndustry />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <FaCode />
+        <GiConveyorBelt />
       </Col>
     </Row>
   );
