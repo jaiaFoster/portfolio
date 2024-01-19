c1666876f2e480b3178e9eb6807b64393a326c48
jaiaFoster <jaiafoster10@gmail.com>
2024-01-18 21:16:18 -0500

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Aboutcard from "./AboutCard";
-import fampic from "../../Assets/fampic.JPG";
+import fampic from "../../Assets/jaiafampic.JPG";
 import AnalysisSimulation from "./AnalysisSimulation"; // Renamed Toolstack to AnalysisSimulation
 import ManufacturingLab from "./ManufacturingLab"; // Renamed Frame to ManufacturingLab
 import DesignModeling from "./DesignModeling"; // Renamed Techstack to DesignModeling
