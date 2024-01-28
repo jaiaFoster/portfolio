34fd030599d5c5b2e77ad7789fb482f92f1e62f3
jaiaFoster <jaiafoster10@gmail.com>
2024-01-28 10:49:14 -0500

    Create Portfolio.js

---
+++ src/components/Portfolio.js
@@ -0,0 +1,14 @@
+// Portfolio.js
+
+import React from 'react';
+
+const Portfolio = () => {
+  return (
+    <div>
+      <h1>My Portfolio</h1>
+      {/* Add your portfolio content here */}
+    </div>
+  );
+}
+
+export default Portfolio;
