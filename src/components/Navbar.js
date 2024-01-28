36a452501038338361cbf4f9fe645eef18d9bd0c
jaiaFoster <jaiafoster10@gmail.com>
2024-01-28 11:01:04 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,6 +10,8 @@ import {
 } from "react-icons/ai";
 
 import { CgFileDocument } from "react-icons/cg";
+import { MdAssignmentInd } from "react-icons/md"; 
+
 
 function NavBar() {
   const [expand, updateExpanded] = useState(false);
@@ -48,6 +50,13 @@ function NavBar() {
         </Navbar.Toggle>
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto" defaultActiveKey="#home">
+            
+            <Nav.Item>
+              <Nav.Link as={Link} to="/portfolio" onClick={() => updateExpanded(false)}>
+                <MdAssignmentInd style={{ marginBottom: "2px" }} /> Portfolio
+              </Nav.Link>
+            </Nav.Item>
+
             <Nav.Item>
               <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
                 <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
