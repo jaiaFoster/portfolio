a5b6a558becf66fe7a2f2bc56a3ba972444b56fa
jaiaFoster <jaiafoster10@gmail.com>
2024-01-29 18:53:20 -0500

    Update Portfolio.js

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,5 +1,5 @@
 // Portfolio.js
-
+import sticky from "../../Assets/sticky-notes.jpeg";
 import React from 'react';
 
 const Portfolio = () => {
@@ -9,9 +9,20 @@ const Portfolio = () => {
         <h1 style={{ fontSize: "2.1em", paddingTop: "100px", paddingBottom: "20px" }}>
         <strong className="purple"> Portfolio</strong>
         </h1>
-        <h2 style={{ fontSize: '24px', marginTop: '0', color: 'white' }}>
+        <h2 style={{ fontSize: '24px', marginTop: '0', color: 'white', textAlign: "left"}}>
           Project 01: Thermocouple Welding Machine
         </h2>
+            <h3 style = {{ fontSize: '12px', textAlign: "left", color: 'white', paddingTop: '10px'}}>
+            Dynamic Systems Inc. or DSI contracted our capstone team at Rensselaer Polytechnic Institute (RPI) with a project: to revitalize and redesign their 40 year old Thermocouple Welding Machine.
+
+            </h3>
+  
+      </div>
+      <div style={{ flex: 1 }}>
+        <img src="sticky" alt="Sticky Notes" style={{ maxWidth: "100%" }} />
+      </div>
+    </div>
+
     </div>
     
   );
