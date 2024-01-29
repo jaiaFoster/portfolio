a7d13eccf7160c275c79532fc2b25a47c4192e5a
jaiaFoster <jaiafoster10@gmail.com>
2024-01-29 18:10:08 -0500

    Update Portfolio.js

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,8 +5,8 @@ import React from 'react';
 const Portfolio = () => {
   return (
     <div>
-      <h1>My Portfolio</h1>
-      {/* Add your portfolio content here */}
+      <h1 style={{ fontSize: '48px', marginBottom: '20px' }}>Portfolio</h1>
+      <h2 style={{ fontSize: '24px', marginTop: '0' }}>Project 01: Thermocouple Welding Machine</h2>
     </div>
   );
 }
